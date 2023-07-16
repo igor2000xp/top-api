82d6d4fdfdc753f2c3466b315de046cffd22e722
igor2000xp <igorring@gmail.com>
2023-07-16 19:38:32 +0200

    feat: add api prefix to main.ts, add draft auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, HttpCode, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @HttpCode(200)
+  @Post('register')
+  async register() {
+
+  }
+
+  @HttpCode(200)
+  @Post('login')
+  async login() {
+
+  }
+
+
+}
